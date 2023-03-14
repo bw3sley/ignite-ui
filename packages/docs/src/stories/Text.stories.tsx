06c499cebb8b3361988b92d22ecc163f8a204ca1
Wesley Bernardes <bw3sley@gmail.com>
2023-03-13 22:19:30 -0300

    :sparkles: feat: create Text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,22 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam vitae sint repudiandae architecto suscipit est exercitationem nihil sed voluptatibus porro ullam laborum dolorem, ducimus, adipisci explicabo omnis praesentium nulla distinctio.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
