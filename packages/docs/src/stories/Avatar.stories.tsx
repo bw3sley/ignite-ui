b99b16a3864ceca74e4a6bc9acc772ac6c4f993a
Wesley Bernardes <bw3sley@gmail.com>
2023-03-20 19:36:35 -0300

    :sparkles: feat: create Avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,21 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://github.com/bw3sley.png',
+    alt: 'Wesley Bernardes',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
