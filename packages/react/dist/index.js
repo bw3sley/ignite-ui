555fd9c5944b7d6c5e8085450e556c4b33709746
Wesley Bernardes <bw3sley@gmail.com>
2023-03-27 22:08:35 -0300

    :sparkles: feat: create TextArea component

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -3166,6 +3166,7 @@ __export(src_exports, {
   Box: () => Box,
   Heading: () => Heading,
   Text: () => Text,
+  TextArea: () => TextArea,
   TextInput: () => TextInput
 });
 module.exports = __toCommonJS(src_exports);
@@ -3419,12 +3420,41 @@ function TextInput(_a) {
     /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(Input, __spreadValues({}, props))
   ] });
 }
+
+// src/components/TextArea.tsx
+var TextArea = styled("textarea", {
+  backgroundColor: "$gray900",
+  padding: "$3 $4",
+  borderRadius: "$sm",
+  boxSizing: "border-box",
+  border: "2px solid $gray900",
+  display: "flex",
+  alignItems: "baseline",
+  fontFamily: "$default",
+  fontSize: "$sm",
+  fontWeight: "$regular",
+  color: "$white",
+  resize: "vertical",
+  minHeight: 80,
+  "&:focus": {
+    outline: 0,
+    borderColor: "$ignite300"
+  },
+  "&:disabled": {
+    opacity: 0.5,
+    cursor: "not-allowed"
+  },
+  "&:placeholder": {
+    color: "$gray400"
+  }
+});
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
   Avatar,
   Box,
   Heading,
   Text,
+  TextArea,
   TextInput
 });
 /*! Bundled license information:
--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -3402,11 +3402,40 @@ function TextInput(_a) {
     /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(Input, __spreadValues({}, props))
   ] });
 }
+
+// src/components/TextArea.tsx
+var TextArea = styled("textarea", {
+  backgroundColor: "$gray900",
+  padding: "$3 $4",
+  borderRadius: "$sm",
+  boxSizing: "border-box",
+  border: "2px solid $gray900",
+  display: "flex",
+  alignItems: "baseline",
+  fontFamily: "$default",
+  fontSize: "$sm",
+  fontWeight: "$regular",
+  color: "$white",
+  resize: "vertical",
+  minHeight: 80,
+  "&:focus": {
+    outline: 0,
+    borderColor: "$ignite300"
+  },
+  "&:disabled": {
+    opacity: 0.5,
+    cursor: "not-allowed"
+  },
+  "&:placeholder": {
+    color: "$gray400"
+  }
+});
 export {
   Avatar2 as Avatar,
   Box,
   Heading,
   Text,
+  TextArea,
   TextInput
 };
 /*! Bundled license information:
